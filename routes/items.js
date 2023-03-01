d740553441f3040b431578762ba6f81293a6543e
amemichelle <54964331+amemichelle@users.noreply.github.com>
2023-03-01 00:55:22 -0800

    created post endpoint to add items to orders

--- routes/items.js
+++ routes/items.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const fs = require("fs");
 const knex = require("knex")(require("../knexfile.js").development);
 
-// Get items purchased by user signed in
 router.get("/purchased", (req, res) => {
   knex
     .select("*")
@@ -13,3 +12,29 @@ router.get("/purchased", (req, res) => {
     });
 });
 module.exports = router;
+
+router.post("/purchased", function (req, res) {
+  let id = req.body.id;
+  let name = req.body.name;
+  let img_src = req.body.img_src;
+  let item_link = req.body.item_link;
+  let price = req.body.price;
+  let specifications = req.body.specifications;
+  let order_id = req.body.order_id;
+  let user_id = req.body.user_id;
+
+  knex("items")
+    .insert({
+      id: id,
+      name: name,
+      img_src: img_src,
+      item_link: item_link,
+      price: price,
+      specifications: specifications,
+      order_id: order_id,
+      user_id: user_id,
+    })
+    .then((data) => {
+      res.json(data);
+    });
+});
