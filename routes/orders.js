985c1e42947d0a9084d94a453b186d4d5eb00cdc
amemichelle <54964331+amemichelle@users.noreply.github.com>
2023-03-02 02:45:46 -0800

    write add new order endpoints

--- routes/orders.js
+++ routes/orders.js
@@ -12,10 +12,46 @@ router.get("/orders", (req, res) => {
     });
 });
 
+router.post("/orders", (req, res) => {
+  let id = req.body.id;
+  let name = req.body.name;
+  let host_id = req.body.host_id;
+  let status = req.body.status;
+
+  knex("orders")
+    .insert({
+      id: id,
+      name: name,
+      host_id: host_id,
+      status: status,
+    })
+    .then((data) => {
+      res.json(data);
+    });
+});
+
 router.get("/participants", (req, res) => {
   knex
     .select("*")
     .from("participants")
+    .then((data) => {
+      res.json(data);
+    });
+});
+
+router.post("/participants", (req, res) => {
+  let id = req.body.id;
+  let user_id = req.body.user_id;
+  let order_id = req.body.order_id;
+  let isHost = req.body.isHost;
+
+  knex("participants")
+    .insert({
+      id: id,
+      user_id: user_id,
+      order_id: order_id,
+      isHost: isHost,
+    })
     .then((data) => {
       res.json(data);
     });
