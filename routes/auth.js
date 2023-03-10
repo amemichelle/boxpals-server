abe742c8b1f66c23c89055581c013d7f2215da97
amemichelle <54964331+amemichelle@users.noreply.github.com>
2023-03-10 15:29:05 -0800

    remove unused dependencies, add catch errors to auth endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -77,6 +77,10 @@ router.post("/login", function (req, res, next) {
     })
     .then((data) => {
       res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error finding user");
     });
 });
 
